a4b70b825eb9c58fb7429f9156260b6733008cb0
seipan1213 <seiju1213@gmail.com>
2022-06-08 23:17:30 +0900

    libft strcmp追加

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/06 14:48:39 by sehattor          #+#    #+#             */
+/*   Updated: 2022/06/08 23:15:49 by sehattor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int ft_strcmp(const char *s1, const char *s2)
+{
+	size_t cnt;
+
+	cnt = 0;
+	while (s1[cnt] && s2[cnt])
+	{
+		if (s1[cnt] != s2[cnt])
+			return ((unsigned char)s1[cnt] - (unsigned char)s2[cnt]);
+		cnt++;
+	}
+	return ((unsigned char)s1[cnt] - (unsigned char)s2[cnt]);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 01:16:23 by sehattor          #+#    #+#             */
-/*   Updated: 2022/05/16 20:13:10 by sehattor         ###   ########.fr       */
+/*   Updated: 2022/06/08 23:13:53 by sehattor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ int					ft_tolower(int c);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_strcmp(const char *s1, const char *s2);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 char				*ft_strnstr(const char *haystack, const char *needle,
